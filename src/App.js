dad05298ae76b38deb2b41e03a62ab5e0da1ed47
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-14 12:07:58 -0600

    Adding a greeting component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
+import { Greeting } from './Greeting';
 
 const App = () => {
   return (
-    <>
-      <h1>Hello my peeps</h1>
-      <h2>Still going</h2>
-      <h2>Yup still going</h2>
-      <h2>Really last one</h2>
-    </>
+    <Greeting />
   );
 }
 
---
+++ src/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Greeting = () => {
+    return (
+        <>
+            <h1>Hello my peeps</h1>
+            <h2>Still going</h2>
+            <h2>Yup still going</h2>
+            <h2>Really last one</h2>
+        </>
+    );
+}

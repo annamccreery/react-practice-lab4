89a4f4efae9346739f1a94c54a9669892cc05899
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-14 19:24:17 -0600

    added favoriteColors and boolean short circ.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Greeting } from './Greeting';
 const App = () => {
   return (
     <>
-      <Greeting name="Anna" favoriteNumber={22} />
+      <Greeting name="Anna" favoriteNumber={22} favoriteColors={["green", "blue", "red"]} />
       <Greeting name="Tamara" favoriteNumber={8} />
       <Greeting name="Tom" favoriteNumber={29} />
       <Greeting name="Andy" favoriteNumber={32} />
--- src/Greeting.js
+++ src/Greeting.js
@@ -7,6 +7,11 @@ export const Greeting = (props) => {
             <h1>Hello {props.name} ! ! !</h1>
             <h2>{props.name}'s favorite number is {props.favoriteNumber}</h2>
             <h3>{props.name}'s favorite number plus 10 is {props.favoriteNumber + 10}</h3>
+            <h3>{props.name}'s favorite colors are: 
+                <ul>
+                    {props.favoriteColors && props.favoriteColors.map(color => <li>{color}</li>)}
+                </ul>
+            </h3>
         </>
     );
 };
